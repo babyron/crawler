f493325d61c4f1170212b1c004c7675246971683
liuming <liuming0430@gmail.com>
2014-04-28 22:46:04 +0800

    删除多余文件

--- host.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse
-
-def get_host(url):
-	url_domain = urllib.parse.urlparse(url)
-	domain = '{uri.scheme}://{uri.netloc}'.format(uri=url_domain)
-	return domain
-
-
-print(get_host("http://baidu.com/123/234"))
